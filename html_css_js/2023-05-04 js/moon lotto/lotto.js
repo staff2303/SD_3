269e91e2c06b54acd8300fbc5d35765ec977c860
staff2303 <staff2303@gmail.com>
2023-05-11 16:57:05 +0900

    2023-05-11 backup

--- html_css_js/2023-05-04 js/moon lotto/lotto.js
+++ html_css_js/2023-05-04 js/moon lotto/lotto.js
@@ -2,66 +2,101 @@ var user_box;
 var chkboxcount;
 var com_child;
 var user_child;
+var chknum;
+var element;
+var user = [];
+var lotto = [];
+var delindex = 0;
 
 window.onload = function seting() {
     user_box = document.getElementById('user_chkbox');
     chkboxcount = 'input[class="user_num"]:checked';
     com_child = document.getElementsByClassName('ball com');
     user_child = document.getElementsByClassName('ball user');
+    chknum = document.getElementsByClassName('user_num');
 }
-// 체크박스 값 가져오기
+// reset
+function reset() {
+    delindex = 0;
+    user = [];
+    lotto = [];
+    showlotto();
+    showuser();
+    document.querySelectorAll(".user_num").forEach(function (v) { v.checked = false; });
+}
+// 체크박스
 function num_check() {
-    let lotto = [];
-    let user = [];
-    const element = document.querySelectorAll(chkboxcount);
-    const chkcount = element.length;
-    let chknum = document.getElementsByClassName('user_num');
-    if (chkcount == 7) {
-        for (let i = 0; i < chknum.length; i++) {
-            if (chknum[i].checked == true) {
-                user.push(Number(chknum[i].value));
-            }
-        }
-        lottonum(lotto);
-        //showlotto 초기화
-        while (com_child.length > 0) {
-            let childelement = document.querySelector("#ball");
-            childelement.remove();
-        }
-        while (user_child.length > 0) {
-            let childelement = document.querySelector("#ball");
-            childelement.remove();
-        }
-        showlotto(lotto);
-        showuser(user);
-        showresult(user, lotto);
+    element = document.querySelectorAll(chkboxcount);
+    if (element.length == 7) {
+        lottonum();            //로또번호 뽑고
+        showlotto();           //로또번호 보여주고
+        showresult();    //결과실행
     } else {
         alert("숫자 7개를 선택해주세요.");
+        reset();
     }
 }
-// 체크박스체크갯수제한
-function count_check() {
-    const element = document.querySelectorAll(chkboxcount);
-    const chkcount = element.length;
-    if (chkcount > 7) {
+function count_check(i) {
+    let index = i - 1;
+    if (chknum[index].checked == true) {
+        if (user.lastIndexOf(Number(chknum[index].value)) == -1) {
+            user.push(Number(chknum[index].value));
+            if(user.length < 7) user.sort((a, b) => a - b);
+            delshowuser();
+            showuser();
+        }
+    } else {
+        for (let i = 0; i < user.length; i++) {
+            if (user[i] === Number(chknum[index].value)) {
+                user.splice(i, 1);
+            }
+        }
+        delshowuser();
+        showuser();
+    }
+    element = document.querySelectorAll(chkboxcount);
+    if (element.length > 7) {
         alert("7개까지 체크할 수 있습니다.");
-        document.querySelectorAll(".user_num").forEach(function (v) { v.checked = false; });
+        reset();
+    }
+}
+//show 초기화
+function delshowlotto() {
+    while (com_child.length > 0) {
+        let childelement = document.querySelector("#ball");
+        childelement.remove();
+    }
+}
+function delshowuser() {
+    while (user_child.length > 0) {
+        let childelement = document.querySelector("#userball");
+        childelement.remove();
     }
 }
 // 로또번호생성
-function lottonum(lotto) {
-    while (lotto.length < 7) {
-        var num = parseInt(Math.random() * 45 + 1);
+function lottonum() {
+    lotto = [];
+    while (lotto.length < 6) {
+        let num = parseInt(Math.random() * 45 + 1);
         if (lotto.indexOf(num) == -1) {
             lotto.push(num);
         }
     }
     // 로또번호정렬
     lotto.sort((a, b) => a - b);
+    // 행운번호
+    while (true) {
+        num = parseInt(Math.random() * 45 + 1);
+        if (lotto.indexOf(num) == -1) {
+            lotto.push(num);
+            break;
+        }
+    }
 }
 // 로또번호출력
-function showlotto(lotto) {
-    for (let i = 0; i < lotto.length; i++) {
+function showlotto() {
+    delshowlotto();
+    for (let i = 0; i < 7; i++) {
         //div 요소 생성
         let newDiv = document.createElement("div");
         //class 지정
@@ -69,46 +104,64 @@ function showlotto(lotto) {
         newDiv.setAttribute("class", "ball com");
         if (i == 6) newDiv.setAttribute("class", "ball com luck");
         //텍스트 입력
-        let newtext = document.createTextNode(lotto[i]);
-        newDiv.appendChild(newtext);
+        if (lotto[i] != undefined) {
+            let newtext = document.createTextNode(lotto[i]);
+            newDiv.appendChild(newtext)
+        };
         //추가
         document.querySelector("#computer_box").append(newDiv);
     }
 }
 // 고른번호출력
-function showuser(user) {
-    for (let i = 0; i < user.length; i++) {
+function showuser() {
+    delshowuser();
+    for (let i = 0; i < 7; i++) {
         //div 요소 생성
         let newDiv = document.createElement("div");
         //class 지정
-        newDiv.setAttribute("id", "ball");
+        newDiv.setAttribute("id", "userball");
         newDiv.setAttribute("class", "ball user");
         if (i == 6) newDiv.setAttribute("class", "ball user luck");
         //텍스트 입력
         let newtext = document.createTextNode(user[i]);
-        newDiv.appendChild(newtext);
+        if (user[i] != undefined) newDiv.appendChild(newtext);
         //추가
         document.querySelector("#user_box").append(newDiv);
     }
 }
 // 결과!!
-function showresult(user, lotto) {
-    console.log(user.filter(x => lotto.includes(x)));
-    switch (user.filter(x => lotto.includes(x)).length) {
-        case 6:
-            alert("1등!!");
+function showresult() {
+    let resultid = document.querySelector(".congratulations");
+    let count = 0;
+    let luck = false;
+    for (let i = 0; i < 6; i++) {
+        if (lotto.indexOf(user[i]) == 1) {console.log(user[i]); count++;}
+    }
+    if (user[6] == lotto[6]) luck = true;
+    console.log("lotto : " + lotto);
+    console.log("user : " + user);
+    console.log("count : " + count);
+    console.log("lucknum : " + luck)
+    switch (count) {
+        case 6: //1등
+            resultid.innerHTML = "1등";
             break;
-        case 5:
-            alert("3등!!");
+        case 5: //2등, 3등
+            if (luck) {
+                resultid.innerHTML = "2등";
+                break;
+            } else {
+                resultid.innerHTML = "3등";
+                break;
+            }
+        case 4: //4등
+            resultid.innerHTML = "4등";
             break;
-        case 4:
-            alert("4등!!");
+        case 3: //5등
+            resultid.innerHTML = "5등";
             break;
-        case 3:
-            alert("5등!!");
-            break;
-        default:
-            alert("꽝!!");
+        default: //꽝
+            resultid.innerHTML = "꽝";
             break;
     }
 }
